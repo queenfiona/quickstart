1bb4133e903ecf728eaf31280002349fdc397a99
queenfiona <fionaityang@gmail.com>
2018-07-17 15:46:51 +0300

    Form validation using vee-vaidate

--- emojis/src/components/Skills.vue
+++ emojis/src/components/Skills.vue
@@ -2,7 +2,8 @@
   <div class="hello">
     <div class="holder">
       <form @submit.prevent="addSkill">
-        <input type="text" placeholder=" Enter a skill that you have.." v-model="skill">              
+        <input type="text" placeholder=" Enter a skill that you have.." v-model="skill" name="skill" v-validate="'min:5'">    
+        <p class="alert" v-if="errors.has('skill')">{{errors.first('skill')}}</p>          
       </form>     
       
       <ul>
@@ -31,8 +32,15 @@ export default {
   },
   methods: {
     addSkill(){
-      this.skills.push({skill:this.skill})
-      this.skill=' ';      
+      this.$validator.validateAll().then((result)=>{
+        if (result){
+          this.skills.push({skill:this.skill})
+          this.skill=' ';  
+        }else{
+          console.log("Not valid")
+        }
+      })
+          
     }
 
   }
@@ -77,5 +85,13 @@ input {
   font-size: 1.3em;
   background-color: #323333;
   /*color: #687F7F;*/
+}
+
+.alert {
+  background: #fdf2ce;
+  font-weight: bold;
+  display: inline-block;
+  padding: 5px;
+  margin-top:  -20px;
 }
 </style>
